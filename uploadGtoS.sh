93245dfabf49919f429f1c730ed5b04f2e3115ed
Rhaynnix <favier-anthony@hotmail.fr>
2020-01-14 16:43:36 +0100

    suppression uploadGtoS.sh a racine (deja present dans communication)

--- uploadGtoS.sh
+++
@@ -1,5 +0,0 @@
-scp ./src/planManager/plans/$1 pi@saturne:/home/pi/$1
-scp ./src/planManager/plans/$1 pi@venus:/home/pi/$1
-## For test
-##scp ./src/planManager/plans/$1 ~/Bureau/TestSatelliteSaturne/$1
-##scp ./src/planManager/plans/$1 ~/Bureau/TestSatelliteVenus/$1
